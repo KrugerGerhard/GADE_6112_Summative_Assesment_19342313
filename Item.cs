231c505256b08157a3587b865065ed0eeaea1008
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-11-30 19:50:03 +0200

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Rougelike_Game
 {
-  public abstract class Item : Tile
+  public abstract class Item : Tile //item class for tile
     {
-        public Item(int x, int y, char Symbol) : base ( x, y, 'I')
+        public Item(int x, int y, char Symbol) : base ( x, y, 'I') //spawn location
         {
 
         }
