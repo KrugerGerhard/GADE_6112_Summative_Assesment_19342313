8bb9aa7a403c758b38ac24cae1413961b5c0bc29
KrugerGerhard <71284936+KrugerGerhard@users.noreply.github.com>
2020-11-30 19:21:08 +0200

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rougelike_Game
+{
+    
+    abstract class Enemy : Character
+    {
+        
+       protected Random generator = new Random();
+
+        public Enemy(int x, int y, int HP, int Damage, char Symbol) : base(x, y,'E')
+        {
+            this.Hp = HP;
+            this.DAMAGE = Damage;
+            
+        }
+
+        public override string ToString()
+        {
+            return "(this.GetType().Name at [{x}, {y}] ({Damage} )";
+        }
+    }
+}
